8f66c3ef3c3080b7e4bceab2e857ab1ca957f759
Ronald van Gimst <gimst@nlr.nl>
2022-11-22 20:50:46 -0500

    Added OTA (compiles & uploads & runs, but to test upload over the air)

--- esp32-Wifi-LEDs/esp32-Wifi-LEDs.ino
+++ esp32-Wifi-LEDs/esp32-Wifi-LEDs.ino
@@ -17,7 +17,12 @@
 
 // Import required WiFi libraries
 #include <WiFiConfig.h>
-#include <WiFi.h>
+#include <WiFi.h> // for both OTA and own functions
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// for the webserver
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
@@ -29,7 +34,7 @@
 #include <FastLED.h>
 
 // FastLED defines
-#define NUM_LEDS 71 // short Strip for inf mirror: 71 LEDs, long reel: 240 LEDs
+#define NUM_LEDS 240 // short Strip for inf mirror: 71 LEDs, long reel: 240 LEDs
 #define DATA_PIN 12 // ESP32-CAM: GPIO12/HS2_DATA3
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
@@ -220,14 +225,59 @@ void setup(){
 //  ledcWrite(ledChannel, slider1Value.toInt());
 
   // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, WiFiPassword);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
     Serial.println("Connecting to WiFi..");
+    delay(1000);
   }
 
+  ////////////////
+  // OTA stuff
+  ////////////////
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+  
   // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
+  Serial.println((String)"IP address: "+WiFi.localIP());
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -287,6 +337,8 @@ void setup(){
 }
   
 void loop() {
+  ArduinoOTA.handle();
+  
   switch (patternSelected) {
     case PAT_MOV_PALETTE:
       // MOVING PALETTE EFFECT
